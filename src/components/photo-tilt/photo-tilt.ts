3d96f779d61cbc403868e69262d05f32d137105d
JooYoo <jooyoo@outlook.com>
2018-09-23 19:10:18 +0200

    try to use ionic scroll

--- src/components/photo-tilt/photo-tilt.ts
+++ src/components/photo-tilt/photo-tilt.ts
@@ -1,43 +1,47 @@
 import { Component, Input, ViewChild, Renderer } from '@angular/core'
-import { Platform, DomController, NavController } from 'ionic-angular'
+import { Platform, DomController, NavController, Content } from 'ionic-angular'
 
 
 @Component({
   selector: 'photo-tilt',
-  templateUrl: 'photo-tilt.html',
-  host: {
-    '(window:deviceorientation)': 'onDeviceOrientation($event)',
-    '(window:resize)': 'initTilt()'
-  }
+  templateUrl:'photo-tilt.html',
+  // host: {
+  //   '(window:deviceorientation)': 'onDeviceOrientation($event)',
+  //   '(window:resize)': 'ngOnInit()'
+  //   // 'initTilt()'
+  // }
 })
 export class PhotoTiltComponent {
 
   // @Input('tiltImage') tiltImage: any;
-  @Input('tiltHeight') tiltHeight: any;
+  // @Input('tiltHeight') tiltHeight: any;
 
-  @ViewChild('mask') mask: any;
-  @ViewChild('image')image: any;
+  // @ViewChild('mask') mask: any;
+  // @ViewChild('image')image: any;
+  // @ViewChild('cardWrapper') cardWrapper: any;
+  @ViewChild('content') content: Content;
+  
 
-  averageGamma: any = [];
-  maxTilt: number = 20;
-  latestTilt: any = 0;
-  centerOffset: any;
-  resizedImageWidth: any;
-  aspectRatio: any;
-  delta: any;
-  height: any;
-  width: any = 0;
+  // averageGamma: any = [];
+  // maxTilt: number = 20;
+  // latestTilt: any = 0;
+  // centerOffset: any;
+  // resizedImageWidth: any;
+  // aspectRatio: any;
+  // delta: any;
+  // height: any;
+  // width: any = 0;
 
   isActive: boolean;
 
-  zoomInX: number
-  zoomInY: number
-  zoomInOffset: number = 0.1
-  isStartPoint: boolean = true
-  startPointX: number
-  nextPointX: number
-  startPointY: number
-  nextPointY: number
+  // zoomInX: number
+  // zoomInY: number
+  // zoomInOffset: number = 0.1
+  // isStartPoint: boolean = true
+  // startPointX: number
+  // nextPointX: number
+  // startPointY: number
+  // nextPointY: number
 
   constructor(public platform: Platform,
     public domCtrl: DomController,
@@ -47,120 +51,131 @@ export class PhotoTiltComponent {
 
   }
 
-  initTilt() {
-
-    this.height = this.tiltHeight || this.platform.height();
-    this.width = this.platform.width();
-    // 长宽比
-    this.aspectRatio = this.image.nativeElement.width / this.image.nativeElement.height;
-    this.renderTilt();
-
+  scrollToRight(){
+    console.log('scrollTo: Hitted!!')
+    this.content.scrollTo(900,0);
   }
 
-  renderTilt() { // 没有这里‘手机倾斜视角动’就不起作用了
-
-    this.image.nativeElement.height = this.height;
-
-    this.resizedImageWidth = this.aspectRatio * this.image.nativeElement.height;
-    this.renderer.setElementStyle(this.image.nativeElement, 'width', this.resizedImageWidth + 'px');
-
-    this.delta = this.resizedImageWidth - this.width;
-    // 中心偏移量；中心起点
-    this.centerOffset = this.delta / 2;
-
-    this.updatePosition();
+  scrollToLeft(){
+    console.log('scrollTo: Hitted!!')
+    this.content.scrollTo(0,500,800);
   }
 
-  onPan(ev) {
-    console.log('onPanX:' + ev.center.x)
-    console.log('onPanY:' + ev.center.y)
+  // mouseDown() {
+  //   this.isActive = true;
+  //   if (this.isActive) {
+  //   //  console.log('isActive:' + 'onPress')
+  //   }
+  // }
 
-    // this.zoomInX = ev.center.x
-    // this.zoomInY = ev.center.y
 
-    // catch the start point //then // get next point each time
-    if (this.isStartPoint) {
-      this.startPointY = ev.center.y
-      this.zoomInY = 1
-    }
-    else {
-      this.nextPointY = ev.center.y
-    }
-    console.log('startPoint:' + this.startPointY)
-    console.log('nextPoint:' + this.nextPointY)
+  // mouseUp() { 
+  //   this.isActive = false
+  //   if (!this.isActive) {
+  //     // console.log('isActive:' + 'onRelease')
+  //   }
+  // }
 
-    if (this.startPointY > this.nextPointY) {
-      this.zoomInY += this.zoomInOffset
-    } else if (this.startPointY < this.nextPointY && !this.isStartPoint) {
-      this.zoomInY -= this.zoomInOffset
-    }
 
-    this.isStartPoint = false
+  // initTilt
+  // ngOnInit() {
 
-    console.log("zoomInY:" + this.zoomInY)
-    console.log('--------------------------------')
+  //   console.log('in initTilt!!!!')
 
-    this.renderer.setElementStyle(this.image.nativeElement, 'transform','scale('+ this.zoomInY + ')');
-    // this.renderer.setElementStyle(this.image.nativeElement, 'height', this.zoomInY + 'px');
+  //   this.height = this.tiltHeight || this.platform.height();
+  //   this.width = this.platform.width();
+  //   // 长宽比
+  //   this.aspectRatio = this.image.nativeElement.width / this.image.nativeElement.height;
+
+  //   console.log('width:' + this.image.nativeElement.width)
+  //   // console.log('cardWrapper:' + this.cardWrapper.nativeElement.width)
+
+
+  //   this.renderTilt();
+
+  // }
+
+  // renderTilt() { // 没有这里‘手机倾斜视角动’就不起作用了
+
+  //   this.image.nativeElement.height = this.height;
+
+  //   this.resizedImageWidth = this.aspectRatio * this.image.nativeElement.height;
+  //   this.renderer.setElementStyle(this.image.nativeElement, 'width', this.resizedImageWidth + 'px');
+
+  //   this.delta = this.resizedImageWidth - this.width;
+  //   // 中心偏移量；中心起点
+  //   this.centerOffset = this.delta / 2;
+
+  //   this.updatePosition();
+  // }
+
+  // onPan(ev) {
+  //   // catch the start point //then // get next point each time
+  //   if (this.isStartPoint) {
+  //     this.startPointY = ev.center.y
+  //     this.zoomInY = 1
+  //   }
+  //   else {
+  //     this.nextPointY = ev.center.y
+  //   }
+
+  //   if (this.startPointY > this.nextPointY) {
+  //     this.zoomInY += this.zoomInOffset
+  //   } else if (this.startPointY < this.nextPointY && !this.isStartPoint) {
+  //     this.zoomInY -= this.zoomInOffset
+  //   }
+
+  //   this.isStartPoint = false
+
+  //   console.log('--------------------------------')
+
+  //   this.renderer.setElementStyle(this.image.nativeElement, 'transform','scale('+ this.zoomInY + ')');
+  //   // this.renderer.setElementStyle(this.image.nativeElement, 'height', this.zoomInY + 'px');
     
-  }
+  // }
 
-  onDeviceOrientation(ev) { // 如果设备出现方向上的变化，这个函数就会被调用
+  // onDeviceOrientation(ev) { // 如果设备出现方向上的变化，这个函数就会被调用
 
-    if (this.isActive) {
+  //   if (this.isActive) {
 
-      if (this.averageGamma.length > 8) {
-        this.averageGamma.shift();
-      }
+  //     if (this.averageGamma.length > 8) {
+  //       this.averageGamma.shift();
+  //     }
 
-      this.averageGamma.push(ev.gamma);
-       console.log("shift:" + this.averageGamma)
+  //     this.averageGamma.push(ev.gamma);
 
-      // 求过去八次的平均值
-      this.latestTilt = this.averageGamma.reduce((previous, current) => {
-        return previous + current;
-      }) / this.averageGamma.length;
+  //     // 求过去八次的平均值
+  //     this.latestTilt = this.averageGamma.reduce((previous, current) => {
+  //       return previous + current;
+  //     }) / this.averageGamma.length;
 
-      this.domCtrl.write(() => {
-        this.updatePosition();
-      });
-    }
-  }
+  //     this.domCtrl.write(() => {
+  //       this.updatePosition();
+  //     });
+  //   }
+  // }
 
   // click to active infinity
-  mouseDown() {
-    this.isActive = true;
-    if (this.isActive) {
-      console.log('isActive:' + 'onPress')
-    }
-  }
+ 
+  // updatePosition() {
 
-  mouseUp() { 
-    this.isActive = false
-    if (!this.isActive) {
-      console.log('isActive:' + 'onRelease')
-    }
-  }
+  //   let tilt = this.latestTilt;
 
-  updatePosition() {
+  //   if (tilt > 0) {
+  //     tilt = Math.min(tilt, this.maxTilt);
+  //   } else {
+  //     tilt = Math.max(tilt, this.maxTilt * -1);
+  //   }
 
-    let tilt = this.latestTilt;
+  //   let pxToMove = (tilt * this.centerOffset) / this.maxTilt;
 
-    if (tilt > 0) {
-      tilt = Math.min(tilt, this.maxTilt);
-    } else {
-      tilt = Math.max(tilt, this.maxTilt * -1);
-    }
+  //   this.updateTiltImage((this.centerOffset + pxToMove) * -1);
 
-    let pxToMove = (tilt * this.centerOffset) / this.maxTilt;
+  // }
 
-    this.updateTiltImage((this.centerOffset + pxToMove) * -1);
+  // updateTiltImage(pxToMove) {
 
-  }
-
-  updateTiltImage(pxToMove) {
-
-    this.renderer.setElementStyle(this.image.nativeElement, 'transform', 'translate3d(' + pxToMove + 'px,0,0)');
-  }
+  //   this.renderer.setElementStyle(this.image.nativeElement, 'transform', 'translate3d(' + pxToMove + 'px,0,0)');
+  // }
 
 }
