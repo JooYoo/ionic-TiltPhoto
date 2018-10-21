7b6cd4a9ad4c56e15ae675ba1765261d339ba0cd
JooYoo <jooyoo@outlook.com>
2018-10-21 19:12:43 +0200

    added cards 50 cards

--- src/components/photo-tilt/photo-tilt.ts
+++ src/components/photo-tilt/photo-tilt.ts
@@ -1,6 +1,6 @@
 import { Component, Renderer } from '@angular/core'
 import { Platform, DomController } from 'ionic-angular'
-import { DeviceMotion, DeviceMotionAccelerationData,DeviceMotionAccelerometerOptions  } from '@ionic-native/device-motion';
+import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@ionic-native/device-motion';
 
 @Component({
   selector: 'photo-tilt',
@@ -10,25 +10,27 @@ export class PhotoTiltComponent {
 
   data: any;
   subscription: any;
-
-  
+  cards: number[] = []
 
   constructor(public platform: Platform,
     public domCtrl: DomController,
     public renderer: Renderer,
     public deviceMotion: DeviceMotion) {
+    for (let index = 0; index < 50; index++) {
+      this.cards.push(index)
+    }
   }
 
   public startMonitor() {
     var option: DeviceMotionAccelerometerOptions = {
       frequency: 200
     };
- 
+
     this.subscription = this.deviceMotion.watchAcceleration(option).subscribe((acceleration: DeviceMotionAccelerationData) => {
       this.data = acceleration;
     });
   }
- 
+
   public stopMonitor() {
     this.subscription.unsubscribe();
   }
