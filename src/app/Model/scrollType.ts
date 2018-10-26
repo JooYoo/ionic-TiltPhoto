c3d47cbe71c135168ae490127ffaa46e2bec3083
JooYoo <jooyoo@outlook.com>
2018-10-26 12:27:59 +0200

    wait for testing

---
+++ src/app/Model/scrollType.ts
@@ -0,0 +1,5 @@
+export enum ScrollType{
+    positive,
+    negative,
+    stay
+}
--- src/components/photo-tilt/photo-tilt.ts
+++ src/components/photo-tilt/photo-tilt.ts
@@ -1,7 +1,7 @@
 import { Component, Renderer, ViewChild } from '@angular/core'
 import { Platform, DomController, Content } from 'ionic-angular'
 import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@ionic-native/device-motion';
-
+import { ScrollType } from '../../app/Model/scrollType'
 
 @Component({
   selector: 'photo-tilt',
@@ -15,6 +15,8 @@ export class PhotoTiltComponent {
   disY: any
   disZ: any
 
+  scrollUnit: number = 5
+
   rawDataPoolX: number[] = []
   rawDataPoolY: number[] = []
   rawDataPoolZ: number[] = []
@@ -24,9 +26,9 @@ export class PhotoTiltComponent {
   poolIndexX: number = 0
   poolIndexY: number = 0
   poolIndexZ: number = 0
-  resultsX: string[] = []
-  resultsY: string[] = []
-  resultsZ: string[] = []
+  resultsX: ScrollType[] = []
+  resultsY: ScrollType[] = []
+  resultsZ: ScrollType[] = []
 
   subscription: any;
   subscription2: any;
@@ -47,7 +49,10 @@ export class PhotoTiltComponent {
   }
 
   //TODO: result as enum
-  getDataPool(rawDataPool: number[], datePool: number[], poolIndex: number, result: string[]) {
+  getDataPool(rawDataPool: number[],
+    datePool: number[],
+    poolIndex: number,
+    results: ScrollType[]) {
 
     while (poolIndex != rawDataPool.length - 1) {
 
@@ -60,21 +65,21 @@ export class PhotoTiltComponent {
         poolIndex++
       }
 
-      this.checkTrend(datePool, result)
+      this.checkTrend(datePool, results)
     }
   }
 
-  checkTrend(sampleNumber: number[], results: string[]) {
+  checkTrend(sampleNumber: number[], results: ScrollType[]) {
 
     if (sampleNumber[1] > sampleNumber[0] && sampleNumber[2] > sampleNumber[1]
       || sampleNumber[0] == sampleNumber[1] && sampleNumber[2] > sampleNumber[1]) {
-      results.push("大")
+      results.push(ScrollType.positive)
     }
     else if (sampleNumber[0] == sampleNumber[1] && sampleNumber[1] == sampleNumber[2] || sampleNumber[0] == sampleNumber[2]) {
-      results.push("平")
+      results.push(ScrollType.stay)
     }
     else {
-      results.push("小")
+      results.push(ScrollType.negative)
     }
   }
 
@@ -97,24 +102,62 @@ export class PhotoTiltComponent {
 
   getherAccData(acceleration: any) {
 
-    // push acc DataPool
+    // push acc data to raw data pool
     this.rawDataPoolX.push(+Number(acceleration.x).toFixed(2))
     this.rawDataPoolY.push(+Number(acceleration.y).toFixed(2))
     this.rawDataPoolZ.push(+Number(acceleration.z).toFixed(2))
-    //
-    this.getDataPool(this.rawDataPoolX,this.dataPoolX, this.poolIndexX, this.resultsX)
-    this.getDataPool(this.rawDataPoolY,this.dataPoolY, this.poolIndexY, this.resultsY)
-    this.getDataPool(this.rawDataPoolZ,this.dataPoolZ, this.poolIndexZ, this.resultsZ)
 
-    // scroll it
-    this.toScroll()
+    // handle raw data to get "direction signal"
+    this.getDataPool(this.rawDataPoolX, this.dataPoolX, this.poolIndexX, this.resultsX)
+    this.getDataPool(this.rawDataPoolY, this.dataPoolY, this.poolIndexY, this.resultsY)
+    this.getDataPool(this.rawDataPoolZ, this.dataPoolZ, this.poolIndexZ, this.resultsZ)
+
+    // scroll 
+    this.getScrollDirectionX(this.resultsX)
+    //FIXME: getScrollDirectionX
+    //this.getScrollDirectionX(this.resultsY)
   }
 
-
-  // FIXME: use the Data in pool to scroll
-  public toScroll(): void {
+  // to scroll the content
+  getScrollDirectionX(scrollActionsX: ScrollType[]): void {
     //this.scroll._scrollContent.nativeElement.scrollTop = this.disZ
-    this.content.scrollTo(0, this.disY * 100)
+    for (let i = 0; i < scrollActionsX.length; i++) {
+      const oneMoveDirectionX = scrollActionsX[i];
+      this.toScrollX(oneMoveDirectionX)
+    }
+  }
+  toScrollX(oneMoveDirectionX: ScrollType) {
+    if (oneMoveDirectionX == ScrollType.positive) {
+      this.content.scrollTo(2, 0, 300)
+    }
+    else if (oneMoveDirectionX == ScrollType.negative) {
+      this.content.scrollTo(-2, 0, 300)
+    } else {
+      console.log("Stay.")
+    }
+  }
+
+  // FIXME: x y need to be change at same time
+  getScrollDirectionY(scrollActionsY: ScrollType[]): void {
+    //this.scroll._scrollContent.nativeElement.scrollTop = this.disZ
+    for (let i = 0; i < scrollActionsY.length; i++) {
+      const oneMoveDirectionY = scrollActionsY[i];
+      this.toScrollY(oneMoveDirectionY)
+    }
+  }
+  toScrollY(oneMoveDirectionY: ScrollType) {
+    if (oneMoveDirectionY == ScrollType.positive) {
+      this.content.scrollTo(0, this.scrollUnit +=5, 300)
+    }
+    else if (oneMoveDirectionY == ScrollType.negative) {
+      this.content.scrollTo(0, 2, 300)
+    } else {
+      console.log("Stay.")
+    }
+  }
+
+  toScroll() {
+    this.content.scrollTo(this.scrollUnit += 5, 0)
   }
 
 }
