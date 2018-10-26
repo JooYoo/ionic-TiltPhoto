f2f57999bea9ac74a2021f54777a6aaef63d253a
JooYoo <jooyoo@outlook.com>
2018-10-26 11:20:50 +0200

    added getDataPool to get direction function

--- src/components/photo-tilt/photo-tilt.ts
+++ src/components/photo-tilt/photo-tilt.ts
@@ -10,19 +10,23 @@ import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOp
 export class PhotoTiltComponent {
 
   accData: any;
-  getAccData: any;
   accTimeStamp: any
   disX: any
   disY: any
   disZ: any
-  isFirstTimeStamp: boolean = true
-  firstTimeStamp: any
 
-  headingAccuracy:any
-  magneticHeading:any
-  oriTimeStamp:any
-  trueHeading:any
-
+  rawDataPoolX: number[] = []
+  rawDataPoolY: number[] = []
+  rawDataPoolZ: number[] = []
+  dataPoolX: number[] = []
+  dataPoolY: number[] = []
+  dataPoolZ: number[] = []
+  poolIndexX: number = 0
+  poolIndexY: number = 0
+  poolIndexZ: number = 0
+  resultsX: string[] = []
+  resultsY: string[] = []
+  resultsZ: string[] = []
 
   subscription: any;
   subscription2: any;
@@ -33,18 +37,46 @@ export class PhotoTiltComponent {
   constructor(public platform: Platform,
     public domCtrl: DomController,
     public renderer: Renderer,
-    public deviceMotion: DeviceMotion,
-    public deviceOrientation: DeviceOrientation) {
+    public deviceMotion: DeviceMotion) {
 
     // generate Cards
     for (let index = 0; index < 50; index++) {
       this.cards.push(index)
     }
-   
-   // 
+
   }
 
-  
+  //TODO: result as enum
+  getDataPool(rawDataPool: number[], datePool: number[], poolIndex: number, result: string[]) {
+
+    while (poolIndex != rawDataPool.length - 1) {
+
+      if (poolIndex > rawDataPool.length - 1) {
+        break
+      }
+
+      for (var i = 0; i < 3; i++) {
+        datePool.push(rawDataPool[poolIndex])
+        poolIndex++
+      }
+
+      this.checkTrend(datePool, result)
+    }
+  }
+
+  checkTrend(sampleNumber: number[], results: string[]) {
+
+    if (sampleNumber[1] > sampleNumber[0] && sampleNumber[2] > sampleNumber[1]
+      || sampleNumber[0] == sampleNumber[1] && sampleNumber[2] > sampleNumber[1]) {
+      results.push("大")
+    }
+    else if (sampleNumber[0] == sampleNumber[1] && sampleNumber[1] == sampleNumber[2] || sampleNumber[0] == sampleNumber[2]) {
+      results.push("平")
+    }
+    else {
+      results.push("小")
+    }
+  }
 
   public startMonitor() {
     var option: DeviceMotionAccelerometerOptions = {
@@ -52,52 +84,37 @@ export class PhotoTiltComponent {
     };
 
     this.subscription = this.deviceMotion.watchAcceleration(option).
-         subscribe((acceleration: DeviceMotionAccelerationData) => {
-      this.accData = acceleration;
-      this.accTimeStamp = acceleration.timestamp 
-      this.getDistance(this.accData,this.accTimeStamp)
-    });
-
-    this.deviceMotion.getCurrentAcceleration().then(
-      (acceleration: DeviceMotionAccelerationData) => {
-        this.getAccData = acceleration
+      subscribe((acceleration: DeviceMotionAccelerationData) => {
+        this.accData = acceleration
+        this.getherAccData(this.accData)
+        // this.accTimeStamp = acceleration.timestamp
       });
-
-
-  }
-
-  getDistance(acceleration: any, currentTimeStamp:any){
-
-    // get first TimeStamp
-    if(this.isFirstTimeStamp){
-      this.firstTimeStamp = currentTimeStamp
-      this.isFirstTimeStamp = false
-    }
-
-    // get Time
-    let disTime = currentTimeStamp - this.firstTimeStamp
-
-    // get 
-    this.disX = Number(acceleration.x).toFixed(0)  
-    this.disY = Number(acceleration.y).toFixed(0) 
-    this.disZ = Number(acceleration.z).toFixed(0) 
-
-    // scroll it
-    this.toScroll()
   }
 
   public stopMonitor() {
     this.subscription.unsubscribe();
   }
 
-  public toScroll(): void {
-    //this.scroll._scrollContent.nativeElement.scrollTop = this.disZ
-    this.content.scrollTo(this.disX *30,this.disZ*30)
+  getherAccData(acceleration: any) {
+
+    // push acc DataPool
+    this.rawDataPoolX.push(+Number(acceleration.x).toFixed(2))
+    this.rawDataPoolY.push(+Number(acceleration.y).toFixed(2))
+    this.rawDataPoolZ.push(+Number(acceleration.z).toFixed(2))
+    //
+    this.getDataPool(this.rawDataPoolX,this.dataPoolX, this.poolIndexX, this.resultsX)
+    this.getDataPool(this.rawDataPoolY,this.dataPoolY, this.poolIndexY, this.resultsY)
+    this.getDataPool(this.rawDataPoolZ,this.dataPoolZ, this.poolIndexZ, this.resultsZ)
+
+    // scroll it
+    this.toScroll()
   }
 
-  
 
-
-
+  // FIXME: use the Data in pool to scroll
+  public toScroll(): void {
+    //this.scroll._scrollContent.nativeElement.scrollTop = this.disZ
+    this.content.scrollTo(0, this.disY * 100)
+  }
 
 }
